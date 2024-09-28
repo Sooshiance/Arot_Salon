e74c23fd29767e1ac8dac543e7096e383a47ac03
sooshiance <s.katafract@gmail.com>
2024-09-28 21:56:03 +0330

    fix: Fix some admin bugs

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -144,7 +144,7 @@ DATABASES = {
 AUTH_USER_MODEL = "user.User"
 
 
-MEDIA_URL = "/media"
+MEDIA_URL = "/media/"
 MEDIA_PATH = BASE_DIR / "media"
 
 
--- user/admin.py
+++ user/admin.py
@@ -5,17 +5,17 @@ from .models import User, Profile
 
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'username', 'is_active',)
+    list_display = ('phone', 'username', 'is_active',)
     filter_horizontal = ()
     list_filter = ('is_active',)
     fieldsets = ()
-    search_fields = ('email', 'username')
-    list_display_links = ('username', 'email')
+    search_fields = ('phone', 'username')
+    list_display_links = ('username', 'phone')
 
 
 class AdminProfile(admin.ModelAdmin):
-    list_display = ['user', 'email', 'pk']
-    search_fields = ('email', 'user')
+    list_display = ['user', 'phone', 'pk']
+    search_fields = ('phone', 'user')
 
 
 admin.site.register(User, UserAdmin)
