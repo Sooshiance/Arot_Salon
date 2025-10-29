2aa5f89e54157774992e1c40ba45c625af9ff9a7
sooshiance <s.katafract@gmail.com>
2025-10-29 19:31:01 +0330

    style: Add more detailed info for Django in mypy.ini

--- backend/apps/account/v1/views.py
+++ backend/apps/account/v1/views.py
@@ -1,5 +1,6 @@
 # TODO: Forget password scenarios
 
+from typing import List
 from django.contrib import auth, messages
 from django.http import (
     HttpRequest,
@@ -15,6 +16,7 @@ from apps.account.v1.forms import LoginForm, RegisterForm
 
 def login_user(
     request: HttpRequest,
+    my_list:List=[],
 ) -> HttpResponseRedirect | HttpResponsePermanentRedirect | HttpResponse:
     if request.user.is_authenticated:
         messages.warning(request, "not this one")
