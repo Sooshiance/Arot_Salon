aa0a000f5494af9bd4abdd1909e685bb7239834c
sooshiance <s.katafract@gmail.com>
2025-11-07 16:06:42 +0330

    feat: Admin panel updated for booking app

--- backend/apps/booking/admin.py
+++ backend/apps/booking/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Departure, ReserveService
+
+
+class DepartureAdmin(admin.ModelAdmin):
+    list_display = ("train",)
+
+
+class ReserveServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "is_active",
+        "admin_approval",
+    )
+    list_filter = (
+        "is_active",
+        "admin_approval",
+    )
+
+
+admin.site.register(Departure, DepartureAdmin)
+
+
+admin.site.register(ReserveService, ReserveServiceAdmin)
